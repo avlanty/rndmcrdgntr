db70b97b80b174b5236da25a8e1bd7b5a55e14ed
Anthony Alvarado <109054924+avlanty@users.noreply.github.com>
2023-10-27 18:52:22 +0000

    p1

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// Define an array of possible card suits and card values
+const suits = ["Hearts", "Spades", "Clubs", "Diamonds"];
+const cardValues = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "King", "Queen", "Jack", "Ace"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generateRandomCard() {
+  // Get a random suit and card value
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomValue = cardValues[Math.floor(Math.random() * cardValues.length)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  // Display the card
+  displayCard(randomValue, randomSuit);
+}
+
+// Function to display the card on the webpage
+function displayCard(value, suit) {
+  const cardSuit = document.getElementById("card-suit");
+  const cardValue = document.getElementById("card-value");
+  
+  cardSuit.textContent = suit;
+  cardValue.textContent = value;
+}
+
+generateRandomCard(); // Call this to display a random card on page load
